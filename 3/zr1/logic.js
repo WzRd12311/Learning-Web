371d3fe888094c3d1c97f0ec9cf3de2f006536cf
Denislav Volin-Danylov <netranner22845@gmail.com>
2025-12-11 20:10:48 +0200

    add index style and var's in logic

---
+++ 3/zr1/logic.js
@@ -0,0 +1,72 @@
+let houseState = 
+[
+    {
+        id: "living",
+        name: " Вітальня",
+        currentTemperature: 22.0,
+        currentHumidity: 45,
+        targetTemperature: 23.0,
+        targetHumidity: 50,
+        ligthON: false,
+        heaterON: false,
+        acON: false, 
+        humidifierON: false,
+        lastActivity: 0
+    },
+    {
+        id: "kitchen",
+        name: "Кухня",
+        currentTemperature: 22.5,
+        currentHumidity: 45,
+        targetTemperature: 21.0,
+        targetHumidity: 50,
+        ligthON: false,
+        heaterON: false,
+        acON: false, 
+        humidifierON: false,
+        lastActivity: 0
+    },
+        {
+        id: "bedroom",
+        name: "Спальня",
+        currentTemperature: 24.0,
+        currentHumidity: 45,
+        targetTemperature: 22.0,
+        targetHumidity: 50,
+        ligthON: false,
+        heaterON: false,
+        acON: false, 
+        humidifierON: false,
+        lastActivity: 0
+    },
+        {
+        id: "bathroom",
+        name: "Ванна",
+        currentTemperature: 22.0,
+        currentHumidity: 35,
+        targetTemperature: 25.0,
+        targetHumidity: 55,
+        ligthON: false,
+        heaterON: false,
+        acON: false, 
+        humidifierON: false,
+        lastActivity: 0
+    },
+        {
+        id: "hall",
+        name: "Коридор",
+        currentTemperature: 17.0,
+        currentHumidity: 35,
+        targetTemperature: 20.0,
+        targetHumidity: 40,
+        ligthON: false,
+        heaterON: false,
+        acON: false, 
+        humidifierON: false,
+        lastActivity: 0
+    },
+]
+
+let isNight = false;      
+let isArmed = false;      
+let outsideTemp = 10;    
